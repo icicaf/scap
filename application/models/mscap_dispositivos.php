071e090d9ce2c3b79180ca0bf5c416c406818809
wallace <c.castillo.inf@gmail.com>
2017-03-27 14:15:39 -0300

    Se agregan funciones para el controlador del dispositivo.

--- application/models/mscap_dispositivos.php
+++ application/models/mscap_dispositivos.php
@@ -55,4 +55,23 @@ class Mscap_dispositivos extends CI_Model
  			return 0;
  		}   	
     }
+
+    //------------------------------------------------------------------------------------------------------------------------
+    // Funcion que verifica el status del dispositivo
+    // para autorizarlo a obtener datos desde el servidor
+    // En este caso permite traer operadores del sistema.
+    public function getOperadores($imei)
+    {
+        //$imei=$datos['dispositivo_imei'];
+        $data=$this->db->query('SELECT * FROM scap_dispositivos WHERE dispositivo_status=1 AND dispositivo_imei='.$imei);
+        $data->num_rows();
+        
+        if($data->num_rows()==1){
+            $query = $this->db->get('scap_operadores');
+            return $query->result_array();
+        }else{
+            echo 'Reqistro no existe o esta duplicado';
+            return 0;
+        }       
+    }
 }
